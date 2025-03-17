e471b122e02b27f345916c6e61c8e620ba4af495
seonho99 <pandapad10@gmail.com>
2025-03-17 14:08:37 +0900

    fix: 연습문제 1-1 set 수정

--- lib/2025-03-15/wizard.dart
+++ lib/2025-03-15/wizard.dart
@@ -1,6 +1,31 @@
+// void main() {
+//   Wand wand = Wand(name: '지팡이', power: 99.9);
+//   Wizard wizard = Wizard('마법사', 100, wand);
+//   wizard.grab(wand);
+// }
+//
+// class Wand {
+//   String name;
+//   double power;
+//
+//   Wand({required this.name, required this.power});
+// }
+//
+// class Wizard {
+//   String name;
+//   int hp;
+//   Wand? wand;
+//
+//   Wizard(this.name, this.hp, this.wand);
+//
+//   void grab(Wand wand) {
+//     print('$name는 ${wand.name}를 들 수 있습니다.');
+//   }
+// }
+
 void main() {
   Wand wand = Wand(name: '지팡이', power: 99.9);
-  Wizard wizard = Wizard('마법사', 100, wand);
+  Wizard wizard = Wizard(name: '마법사', hp: 100);
   wizard.grab(wand);
 }
 
@@ -14,9 +39,11 @@ class Wand {
 class Wizard {
   String name;
   int hp;
-  Wand? wand;
+  Wand? _wand;
 
-  Wizard(this.name, this.hp, this.wand);
+  Wizard({required this.name, required this.hp, Wand? wand}) : _wand = wand;
+
+  set wand(Wand value) => _wand = value;
 
   void grab(Wand wand) {
     print('$name는 ${wand.name}를 들 수 있습니다.');
