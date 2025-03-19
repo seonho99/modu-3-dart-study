4a2ea528183224c10e783614c646d44b94d618ba
seonho99 <pandapad10@gmail.com>
2025-03-19 09:07:48 +0900

    feat: 20250319

--- lib/2025-03-18/great_wizard.dart
+++ lib/2025-03-18/great_wizard.dart
@@ -5,17 +5,18 @@ class GreatWizard extends Wizard {
   int mp = 150;
 
   GreatWizard({required String name, required int hp, required Wand? wand})
-    : super(name: name, hp: hp, wand: wand);
+      : super(name: name, hp: hp, wand: wand);
 
   int _superHeal = 50;
 
   @override
   void heal(Hero hero) {
     super.heal(hero);
-    if (_superHeal <= mp) {
+    final int superHealCost = 40;
+    if (superHealCost <= mp) {
       hero.hp = hero.maxHp;
       print('슈퍼 힐을 시전했습니다. 대상 HP: ${hero.hp}');
-      mp -= 40;
+      mp -= superHealCost;
     } else {
       print('마나가 부족합니다.');
     }
--- test/2025-03-18/poison_slime_test.dart
+++ test/2025-03-18/poison_slime_test.dart
@@ -13,29 +13,37 @@ void main() {
       expect(hero.hp, 72);
     });
     test('poison Count: b,c,e', () {
-      poisonSlime.attack(hero);
-      int firstAttack = poisonSlime.poisonCount;
+      // poisonSlime.attack(hero);
+      // int firstAttack = poisonSlime.poisonCount;
+      //
+      // poisonSlime.attack(hero);
+      // int secondAttack = poisonSlime.poisonCount;
+      //
+      // poisonSlime.attack(hero);
+      // int thirdAttack = poisonSlime.poisonCount;
+      //
+      // poisonSlime.attack(hero);
+      // int fourthAttack = poisonSlime.poisonCount;
+      //
+      // poisonSlime.attack(hero);
+      // int fifthAttack = poisonSlime.poisonCount;
+      //
+      // poisonSlime.attack(hero);
+      //
+      // expect(poisonSlime.poisonCount, equals(0));
+      // expect(firstAttack, equals(4));
+      // expect(secondAttack, equals(3));
+      // expect(thirdAttack, equals(2));
+      // expect(fourthAttack, equals(1));
+      // expect(fifthAttack, equals(0));
+      expect(poisonSlime.poisonCount, equals(5));
 
+      for (int expected = 4; expected >= 0; expected--) {
+        poisonSlime.attack(hero);
+        expect(poisonSlime.poisonCount, equals(expected));
+      }
       poisonSlime.attack(hero);
-      int secondAttack = poisonSlime.poisonCount;
-
-      poisonSlime.attack(hero);
-      int thirdAttack = poisonSlime.poisonCount;
-
-      poisonSlime.attack(hero);
-      int fourthAttack = poisonSlime.poisonCount;
-
-      poisonSlime.attack(hero);
-      int fifthAttack = poisonSlime.poisonCount;
-
-      poisonSlime.attack(hero);
-
       expect(poisonSlime.poisonCount, equals(0));
-      expect(firstAttack, equals(4));
-      expect(secondAttack, equals(3));
-      expect(thirdAttack, equals(2));
-      expect(fourthAttack, equals(1));
-      expect(fifthAttack, equals(0));
     });
   });
 }
