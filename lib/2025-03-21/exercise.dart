60653edc8c3f1ff7dc6317f4d01a3e5e8f540620
seonho99 <pandapad10@gmail.com>
2025-03-21 20:22:30 +0900

    feat: 연습문제 2.

--- lib/2025-03-21/exercise.dart
+++ lib/2025-03-21/exercise.dart
@@ -16,6 +16,11 @@ class Book {
 
   @override
   int get hashCode => title.hashCode ^ publishDate.hashCode;
+
+  @override
+  String toString() {
+    return 'Book{title: $title, publishDate: $publishDate, comment: $comment}';
+  }
 }
 
 void main() {
@@ -27,8 +32,28 @@ void main() {
 
   Book book2 = Book(
     title: '한국책',
-    comment: '댓글1',
+    comment: '댓글2',
     publishDate: DateTime(24, 01, 01),
   );
   print(book1 == book2);
+
+  Book book3 = Book(
+    title: '미국책',
+    comment: '댓글3',
+    publishDate: DateTime(23, 03, 13),
+  );
+
+  Book book4 = Book(
+    title: '중국책',
+    comment: '댓글4',
+    publishDate: DateTime(21, 12, 25),
+  );
+
+  final books = [book1, book2, book3, book4];
+
+  final byPublishDate = books.toList()..sort((a,b) => a.publishDate.compareTo(b.publishDate));
+
+
+  print(book1 == book2);
+  print(byPublishDate);
 }
