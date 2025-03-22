ef56964c9db610e9a5708f73dea84c300cf127dc
seonho99 <pandapad10@gmail.com>
2025-03-22 13:28:02 +0900

    featL 2)

--- lib/2025-03-21/exercise.dart
+++ lib/2025-03-21/exercise.dart
@@ -21,39 +21,13 @@ class Book {
   String toString() {
     return 'Book{title: $title, publishDate: $publishDate, comment: $comment}';
   }
+
+  Book copyWith({String? title, String? comment, DateTime? publishDate}) {
+    return Book(
+      title: title ?? this.title,
+      comment: comment ?? this.comment,
+      publishDate: publishDate ?? this.publishDate,
+    );
+  }
 }
 
-void main() {
-  Book book1 = Book(
-    title: '한국책',
-    comment: '댓글1',
-    publishDate: DateTime(24, 01, 01),
-  );
-
-  Book book2 = Book(
-    title: '한국책',
-    comment: '댓글2',
-    publishDate: DateTime(24, 01, 01),
-  );
-  print(book1 == book2);
-
-  Book book3 = Book(
-    title: '미국책',
-    comment: '댓글3',
-    publishDate: DateTime(23, 03, 13),
-  );
-
-  Book book4 = Book(
-    title: '중국책',
-    comment: '댓글4',
-    publishDate: DateTime(21, 12, 25),
-  );
-
-  final books = [book1, book2, book3, book4];
-
-  final byPublishDate = books.toList()..sort((a,b) => a.publishDate.compareTo(b.publishDate));
-
-
-  print(book1 == book2);
-  print(byPublishDate);
-}
---
+++ test/2025-03-21/exercise_test.dart
@@ -0,0 +1,46 @@
+import 'package:modu_3_dart_study/2025-03-21/exercise.dart';
+import 'package:test/test.dart';
+
+void main() {
+  Book book1 = Book(
+    title: '한국책',
+    comment: '댓글1',
+    publishDate: DateTime(2024, 01, 01),
+  );
+
+  Book book2 = Book(
+    title: '한국책',
+    comment: '댓글2',
+    publishDate: DateTime(2024, 01, 01),
+  );
+
+  Book book3 = Book(
+    title: '미국책',
+    comment: '댓글3',
+    publishDate: DateTime(2023, 03, 13),
+  );
+
+  Book book4 = Book(
+    title: '중국책',
+    comment: '댓글4',
+    publishDate: DateTime(2021, 12, 25),
+  );
+
+  final books = [book1, book2, book3, book4];
+
+  final byPublishDate =
+  books.toList()..sort((a, b) => a.publishDate.compareTo(b.publishDate));
+
+  group('book test', () {
+    test('출간일 동일', () {
+      expect(book2 == book1, true);
+    });
+
+    test('출간일 sort', () {
+      expect(byPublishDate[0].publishDate, DateTime(2021, 12, 25));
+      expect(byPublishDate[1].publishDate, DateTime(2023, 03, 13));
+      expect(byPublishDate[2].publishDate, DateTime(2024, 01, 01));
+      expect(byPublishDate[3].publishDate, DateTime(2024, 01, 01));
+    });
+  });
+}
