22ce30d995f7e14a0dd7747abbd854da28997eb4
seonho99 <pandapad10@gmail.com>
2025-03-24 19:45:33 +0900

    feat 문자열 과제

---
+++ lib/2025-03-24/exercise_word.dart
@@ -0,0 +1,21 @@
+class Word {
+  String word;
+
+  Word(this.word);
+
+  bool isVowel(int i) {
+    if (i < 0 || i >= word.length) {
+      return false;
+    }
+    String vowel = word[i].toLowerCase();
+    return 'aeiou'.contains(vowel);
+  }
+
+  bool isConsonant(int i) {
+    if (i < 0 || i >= word.length) {
+      return false;
+    }
+    String consonant = word[i].toLowerCase();
+    return !'aeiou'.contains(consonant) && consonant.isNotEmpty;
+  }
+}
---
+++ test/2025-03-24/exercise_word.dart
@@ -0,0 +1,19 @@
+import 'package:modu_3_dart_study/2025-03-24/exercise_word.dart';
+import 'package:test/test.dart';
+
+void main() {
+  Word vowel = Word('vowel');
+  Word consonant = Word('consonant');
+
+  group('낱말 테스트', () {
+    test('모음 확인', () {
+      expect(vowel.isVowel(1), equals(false));
+      expect(vowel.isVowel(2), equals(true));
+    });
+    test('자음 확인', () {
+      expect(consonant.isConsonant(1), equals(true));
+      expect(consonant.isConsonant(2), equals(false));
+    });
+  });
+
+}
