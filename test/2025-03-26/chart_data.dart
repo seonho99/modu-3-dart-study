b35e308a7b8b5e93ba8fd58968df1c3fbacc8e9c
seonho99 <pandapad10@gmail.com>
2025-03-27 08:43:03 +0900

    feat: json test 코드 작성

--- test/2025-03-26/chart_data.dart
+++ test/2025-03-26/chart_data.dart
@@ -1,19 +1,25 @@
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:modu_3_dart_study/2025-03-26/chart_data.dart';
 import 'package:test/test.dart';
 
 void main() {
-  File file = File('lib/2025-03-26/chart_data.json');
 
-  String listData = file.readAsStringSync();
+  test('json 데이터를 읽고 처리', () {
+    File file = File('lib/2025-03-26/chart_data.json');
 
-  Map<String, dynamic> decode = jsonDecode(listData);
+    final String jsonString = file.readAsStringSync();
+    final jsonMap = jsonDecode(jsonString);
+    final dataList = CollectionCharDataList.fromJson(jsonMap);
 
+    expect(dataList.collectionCharDataList, isNotEmpty);
+    expect(dataList.collectionCharDataList.first.collectionName, isNotEmpty);
 
-  test('', (){
-
-
+    final firstCollection = dataList.collectionCharDataList.first;
+    expect(firstCollection.collectionSalePrice, isNotEmpty);
+    expect(firstCollection.collectionSalePrice.first.price, greaterThan(0));
+    expect(firstCollection.collectionSalePrice.first.cvtDatetime, isA<DateTime>());
 
   });
 }
