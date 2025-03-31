a00eee83ebdd1f2ee458fd0e6e2b0919d0201970
seonho99 <pandapad10@gmail.com>
2025-03-31 14:44:33 +0900

    feat: 연습문제 2

--- lib/2025-03-31/data_source/todo_data_source_impl.dart
+++ lib/2025-03-31/data_source/todo_data_source_impl.dart
@@ -1,14 +1,24 @@
 import 'dart:convert';
-
+import 'todo.dart';
+import 'todo_data_source.dart';
 
 class TodoDataSourceImpl implements TodoDataSource {
   final String jsonString;
+  final String jsonList;
 
-  TodoDataSourceImpl({required this.jsonString});
+  TodoDataSourceImpl({required this.jsonString, required this.jsonList});
 
   @override
   Future<Todo> getTodo() async {
     final Map<String, dynamic> json = jsonDecode(jsonString);
     return Todo.fromJson(json);
+  }
+
+  @override
+  Future<List<Todo>> getTodos() async {
+    final List<Map<String, dynamic>> jsonStrings = jsonDecode(jsonList);
+    return jsonStrings
+        .map((Map<String, dynamic> json) => Todo.fromJson(json))
+        .toList();
   }
 }
